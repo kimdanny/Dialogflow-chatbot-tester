750a857e386b2f31df7038da5ba7e3ae4e415348
Danny Kim <dannykim153@gmail.com>
2020-03-30 23:05:53 +0900

    file separation and send request

--- test.py
+++ test.py
@@ -5,26 +5,29 @@ import requests
 
 API_END_POINT = "http://localhost:5000/api/inputText"
 
-generateConversation = GenerateConversation()
+generateConversation = GenerateConversation("MHRA.csv")
 conversation = generateConversation.generate()
+print(conversation)
 # TODO: process user message
-userMessage = ""
+userMessage = "hi"
 body = {
         "message"   : userMessage,
         "sessionID" : "ryan"
         }
 header = {"Content-Type": "application/json"}
 
+# Method 1
 r = requests.post(API_END_POINT, json=body, headers=header)
 print(r.text)
 
+# Method 2
 # req = urllib.request.Request(API_END_POINT)
-# req.add_header('Content-Type', 'application/json; charset=utf-8')
+# req.add_header('Content-Type', 'application/json')
 # jsondata = json.dumps(body)
 # jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-# req.add_header('Content-Length', len(jsondataasbytes))
+# # req.add_header('Content-Length', len(jsondataasbytes))
 # print (jsondataasbytes)
 # response = urllib.request.urlopen(req, jsondataasbytes)
-
+# print(response)
 
 # TODO: Compare
